2af371444938099208caee93a521a86da9823439
Bojan Mirosavljević <bojan.miros@gmail.com>
2024-12-10 01:31:18 +0100

    rework menu scene and add models for level base

--- Assets/CoreSystems/Scripts/Managers/GameManager.cs
+++ Assets/CoreSystems/Scripts/Managers/GameManager.cs
@@ -1,36 +1,42 @@
+using System.Collections.Generic;
+using System.ComponentModel;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : StaticInstance<GameManager>
 {
-    [SerializeField] private TextMeshProUGUI killCounterText;
-
     private void Start()
     {
-        DUMMY_RefreshEnemyKillCounter();
-    }
+        Level level = new Level();
+        level.Stages = new List<Stage>();
 
-    //referenced by button in scene
-    public void DUMMY_GoToGameScene()
-    {
-        SceneSystem.Instance.LoadScene(
-            Scene.Loading,
-            () =>
-            {
-                Debug.LogError("SceneChanged");
-            }
-        );
-    }
+        Stage introStage = new StageIntro();
+        var intro1 = new StepDialogue(); //dialog
+        intro1.PersonLeft = "DialoguePlayer";
+        intro1.PersonRight = "DialogueHR";
+        intro1.Messages = new List<DialogueText>()
+        {
+            new DialogueText(DialogueDirection.Right, "Dobro došli!\nImali smo nekoliko manjih incidenata, pa smo zamolili za dodatnu inspekciju.\nMolim Vas da obratite pažnju na to da li su sva pravila ispostovana."),
+            new DialogueText(DialogueDirection.Left, "Vazi, istrazicu!"),
+            new DialogueText(DialogueDirection.Right, "Prvo mozete proci kroz pravilnik za sigurnosnu opremu, izvolite!"),
+        };
 
-    //referenced by button in scene
-    public void DUMMY_KillEnemy()
-    {
-        SaveSystem.Instance.Player.EnemyKillCounter++;
-        DUMMY_RefreshEnemyKillCounter();
-    }
+        var intro2 = new StepShowNotes();
+        intro2.Image = "NotesObaveze";
 
-    private void DUMMY_RefreshEnemyKillCounter()
-    {
-        killCounterText.text = "Counter: " + SaveSystem.Instance.Player.EnemyKillCounter;
+        var intro3 = new StepDialogue(); //dialog
+        intro3.PersonLeft = "DialoguePlayer";
+        intro3.PersonRight = "DialogueHR";
+        intro3.Messages = new List<DialogueText>()
+        {
+            new DialogueText(DialogueDirection.Right, "Pokazacu Vam sigurnosne kamere radnog mesta od pre par sati.\nMolim Vas obelezite sve nepravilnosti.")
+        };
+
+        introStage.Steps.Add(intro1);
+        introStage.Steps.Add(intro2);
+        introStage.Steps.Add(intro3);
+
+        level.Stages.Add(introStage);
     }
 }
--- Assets/CoreSystems/Scripts/Managers/LoadingManager.cs
+++ Assets/CoreSystems/Scripts/Managers/LoadingManager.cs
@@ -1,42 +1,37 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoadingManager : StaticInstance<LoadingManager>
 {
-    [SerializeField] private Button DUMMY_Button;
-
+    [SerializeField] List<Button> Levels;
+    
     private void Start()
     {
-        DUMMY_Button.interactable = false;
-        DUMMY_Button.onClick.AddListener(DUMMY_GoToGameScene);
-
         StartCoroutine(LoadingQueue());
     }
 
     private IEnumerator LoadingQueue()
     {
         yield return new WaitUntil(() => Systems.Instance.IsInitialized());
-        
-        QueueSystem.Instance.Enqueue(new QueueDelay(2f));
-        QueueSystem.Instance.Enqueue(new CustomQueueItem(EnableButton));
+
+        SetupLevelButtons();
 
         QueueSystem.Instance.RunNext();
     }
 
-    private void EnableButton()
+    private void SetupLevelButtons()
     {
-        DUMMY_Button.interactable = true;
-    }
-    
-    public void DUMMY_GoToGameScene()
-    {
-        SceneSystem.Instance.LoadScene(
-            Scene.Game,
-            () =>
-            {
-                Debug.LogError("SceneChanged");
-            }
-        );
+        Levels[0].onClick.AddListener(() =>
+        {
+            SceneSystem.Instance.LoadScene(
+                Scene.Game,
+                () =>
+                {
+                    Debug.LogError("Level 0 Loaded");
+                }
+            );
+        });
     }
 }
